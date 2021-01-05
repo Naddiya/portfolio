415ee9096f89fff439247abc8728cb748d2cb5c0
Naddiya <nadiamedkouri@gmail.com>
2021-01-05 17:37:23 +0100

    Image removed from about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,12 +2,11 @@ import React, { useContext, useState, useEffect } from 'react';
 import Fade from 'react-reveal/Fade';
 import { Container, Row, Col } from 'react-bootstrap';
 import Title from '../Title/Title';
-import AboutImg from '../Image/AboutImg';
 import PortfolioContext from '../../context/context';
 
 const About = () => {
   const { about } = useContext(PortfolioContext);
-  const { img, paragraphOne, paragraphTwo, paragraphThree, paragraphFour, resume } = about;
+  const { paragraphOne, paragraphTwo, paragraphThree, paragraphFour, resume } = about;
 
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
@@ -27,11 +26,11 @@ const About = () => {
       <Container>
         <Title title="WHOAMI" />
         <Row className="about-wrapper">
-          <Col md={6} sm={12}>
+          <Col md={1} sm={8}>
             <Fade bottom duration={1000} delay={600} distance="30px">
-              <div className="about-wrapper__image">
+              {/* <div className="about-wrapper__image">
                 <AboutImg alt="profile picture" filename={img} />
-              </div>
+              </div> */}
             </Fade>
           </Col>
           <Col md={6} sm={12}>
